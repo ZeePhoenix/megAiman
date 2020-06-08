c5555b5dd034d59b3ba7d82e7a155c8cc94f0035
Zac <zcupec@gmail.com>
2020-06-08 01:09:19 -0400

    Added basic Neural Network
    Can create a layer of neurons, and can feed forward our input data

--- pyBrain.py
+++ pyBrain.py
@@ -1,10 +1,13 @@
 import socket
 import json
 import time
+import threading
 import numpy as np
 
 bizhawk = None
 HOST, PORT = "192.168.1.133", 8852
+
+Brain = []
 
 class Server:
 	serverLoop = None
@@ -24,22 +27,7 @@ class Server:
 		bizhawk.settimeout(5)
 		print("Connected to Emulator! ", addr)
 		Server.serverLoop = Server.startServerLoop()
-
-	@classmethod
-	def serverLoop(self):
-		romName = (bizhawk.recv(48)).decode('utf-8')
-		print("Playing: ", romName)
-		while True:
-			try:
-				# Get our training data from bizhawk
-				X = (bizhawk.recv(512)).decode('utf-8')
-				print(X)
-				# Evaluate our Training Data
-				# Send the evaluated Joypad Data to bizhawk
-				pass
-			except socket.timeout:
-				pass
-			time.sleep(0.01)
+		pass
 
 	@classmethod
 	def startServerLoop(self):
@@ -47,6 +35,58 @@ class Server:
 		t.daemon = True
 		t.start()
 		return t
+
+	@classmethod
+	def serverLoop(self):
+		romName = (bizhawk.recv(48)).decode('utf-8')
+		print("Playing: ", romName)
+		Network.initalizeNetwork()
+		while True:
+			try:
+				# Get our training data from bizhawk
+				X = [123,0,1,24,20] #Temp Data
+				# Evaluate our Training Data
+				print(Network.feedForward(X))
+				# Send the evaluated Joypad Data to bizhawk
+				pass
+			except socket.timeout:
+				pass
+			time.sleep(0.01)
+		pass
+
+
+class Layer_Dense:
+	def __init__(self, inputs, neurons):
+		self.weights = 0.1 * np.random.randn(inputs, neurons)
+		self.biases = np.full((1, neurons), 0.0)
+		pass
+	def forward(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+		pass
+
+class Network:
+	global Brain
+
+	@classmethod
+	def initalizeNetwork(self):
+		# Creates our Neural Network with 2 Hidden Layers
+		# Input layer
+		Brain.append(Layer_Dense(5,8))
+		# Hidden Layers
+		Brain.append(Layer_Dense(8,8))
+		Brain.append(Layer_Dense(8,8))
+		# Output Layer - Gives our joypad signal
+		Brain.append(Layer_Dense(8,6))
+		pass
+	@classmethod
+	def feedForward(self, X):
+		Brain[0].forward(X)
+		Brain[1].forward(Brain[0].output)
+		Brain[2].forward(Brain[1].output)
+		Brain[3].forward(Brain[2].output)
+		return Brain[3].output
+
+
 
 Server.awaitConnection()
 while True:
